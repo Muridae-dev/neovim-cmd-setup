a1fe0f4c1e268c7969e136e7ea013e02904e8a2c
Muridae-dev <92300249+Muridae-dev@users.noreply.github.com>
2026-04-07 14:52:22 +0200

    add change to dark-mode settings when dark-mode is set

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -74,7 +74,17 @@ return {
       lazy = false,
       priority = 1000,
       config = function()
-        vim.cmd.colorscheme("noctis-lux")
+        local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
+        local result = handle:read("*a")
+        handle:close()
+
+        if result:match("Dark") then
+          vim.o.background = "dark"
+          vim.cmd.colorscheme("noctis")
+        else
+          vim.o.background = "light"
+          vim.cmd.colorscheme("noctis-lux")
+        end
       end,
     },
     {
